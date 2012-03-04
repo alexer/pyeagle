404a20a771b7f96350e46e885a0ce6da362c45c8
Aleksi Torhamo <aleksi@torhamo.net>
2012-03-04 08:51:44 +0200

    More info on the layer data

--- eagle.py
+++ eagle.py
@@ -10,9 +10,7 @@ def dump_dec_hex_ascii(data):
 		print ' '.join(' %s ' % (byte if 32 <= ord(byte) <= 127 else '.',) for byte in data)
 		print
 
-with file(sys.argv[1]) as f:
-	f.read(6*16)
-
+def read_layers(f):
 	while True:
 		data = f.read(4)
 		if not data:
@@ -21,25 +19,41 @@ with file(sys.argv[1]) as f:
 		if data == '\x13\x12\x99\x19':
 			break
 		# All values before the sentinel have zero in the second byte (except untiled, it has 128)
-		if data[1] != '\x00' and (data[0] not in '\x15\x18' or data[1] != '\x80'):
+		if data[1] != '\x00' and (data[0] not in '\x15\x17\x18\x19\x37' or data[1] != '\x80'):
 			break
 		data += f.read(20)
-		if data[0] in '\x15\x18' and data[1] == '\x80':
+		if data[0] in '\x15\x17\x18\x19' and data[1] == '\x80':
 			assert data[15:] == '\x00untitled'
 
+		#0x13 = layer
+		#0x1d = symbol
+		#0x1e = package
+		#0x37 = device
+		#0x36 = ???
 		if data[0] == '\x13':
-			c1, c2, flags, layer, opposite_layer, xxx2, xxx3 = struct.unpack('BBBBBBB', data[:7])
+			c1, c2, flags, layer, opposite_layer, fill, color = struct.unpack('BBBBBBB', data[:7])
+			name = data[15:].strip('\x00')
 			assert c1 == 0x13
 			assert c2 == 0x00
 			assert data[7:15] == 8*'\x00'
 			side = 'bottom' if flags & 0x10 else 'top'
-			flags &= ~0x10
-			print 'Layer: side=%s, layer=%d, other=%d, xxx1=%d, xxx2=%d, xxx3=%d, name=%s' % (side, layer, opposite_layer, flags, xxx2, xxx3, data[15:].strip('\x00'))
+			visible = {3: 0, 13: 0, 14: 1, 15: 1}[flags & (~0x10)]
+			print 'Layer: fill=%d, color=%d, name=%s, layer=%d, other=%d, side=%s, unknown_flags=%d, visible=%d' % (fill, color, name, layer, opposite_layer, side, flags, visible)
 		else:
 			dump_hex_ascii(data)
 
+	dump_hex_ascii(data)
 	assert data == '\x13\x12\x99\x19'
+
+def read_name_array(f):
 	size = struct.unpack('<I', f.read(4))[0]
 	print size, f.read(size).split('\x00')
+
+with file(sys.argv[1]) as f:
+	data = f.read(6*16)
+	#print ''.join('%02x' % (ord(byte),) for byte in data)
+
+	read_layers(f)
+	read_name_array(f)
 	dump_hex_ascii(f.read())
 
