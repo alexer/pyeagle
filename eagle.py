a636a7af65f57c536164f419c47c42ff70634999
Aleksi Torhamo <aleksi@torhamo.net>
2012-03-05 06:42:15 +0200

    Playing with arcs

--- eagle.py
+++ eagle.py
@@ -63,8 +63,18 @@ def read_layers(f):
 			visible = {3: 0, 13: 0, 14: 1, 15: 1}[flags & (~0x10)]
 			print '- Layer: fill=%d, color=%d, name=%s, layer=%d, other=%d, side=%s, unknown_flags=%d, visible=%d' % (fill, color, name, layer, opposite_layer, side, flags, visible)
 		elif data[0] == '\x22':
-			layer, x1, y1, x2, y2, hw = struct.unpack('<biiiiI', data[3:])
+			layer, x1, y1, x2, y2, hw = struct.unpack('<bIIIII', data[3:])
+			c = struct.unpack('<I', data[7:20:4])[0]
+			x1, y1, x2, y2 = [val & 0xffffff for val in (x1, y1, x2, y2)]
 			print '- Line from (%f", %f") to (%f", %f"), width %f", layer %d' % (u2in(x1), u2in(y1), u2in(x2), u2in(y2), u2in(hw*2), layer)
+			x3, y3 = (x1+x2)/2., (y1+y2)/2.
+			if x2-x1 < y2-y1:
+				cx = c
+				cy = (x3-cx)*(x2-x1)/float(y2-y1)+y3
+			else:
+				cy = c
+				cx = (y3-cy)*(y2-y1)/float(x2-x1)+x3
+			print 'Arc center at:', cx/2.54, cy/2.54
 			dump_hex_ascii(data[7::4])
 		elif data[0] == '\x25':
 			layer, x1, y1, r1, r2, hw = struct.unpack('<biiiiI', data[3:])
